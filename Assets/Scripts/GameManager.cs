f434dc4ebce5e4ee7e8d86d951149d187906d571
CJnovack <cnovack@student.fitchburgstate.edu>
2025-03-31 16:45:54 -0400

    Possibly Penultimate Update
    Everything is nearly finished. Game is playable, there's just some crap on the screen I'm trying to make disappear and the game can't exactly end yet.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject titleScreen;
+    public Button startButton;
+    public TextMeshProUGUI gameEndText;
+    public Button restartButton;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameEndText.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
@@ -19,5 +25,32 @@ public class GameManager : MonoBehaviour
     public void StartGame()
     {
         titleScreen.gameObject.SetActive(false);
+        Time.timeScale = 1f;
+        startButton.gameObject.SetActive(false);
+    }
+
+    private void Awake()
+    {
+        Time.timeScale = 0f;
+    }
+
+    private void OnEnable()
+    {
+        startButton.onClick.AddListener(StartGame);
+    }
+
+    private void OnDisable()
+    {
+        startButton.onClick.RemoveListener(StartGame);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void GameEnd()
+    {
+        restartButton.gameObject.SetActive(true);
     }
 }
